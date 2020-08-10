3cb2d31ec109769990ff8a45c1802c29e218e236
Feroz Basheer <feroz@outlook.com>
2020-08-10 15:41:37 -0700

    Set up basic interrupts
     - Set up IDT
     - Set up basic keyboard interrupt handling

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/interrupts.h>
 
 void kernel_main(void) {
+	idt_initialize();
 	terminal_initialize();
 	printf("Quilon OS v0.0.1\r\n");
 	printf("\r\n");
 
-	int i = 1;
-	while(i) {
-		printf("%c %s ", 'c' , "this");
-	}
+
+	//while(1);
+
+	////int i = 1;
+	//while(i) {
+		printf("%c %s %d", 'c' , "this", 42);
+	//}
+
+	while(1);
 }
