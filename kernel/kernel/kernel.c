66ea2396d7f3acb508f6131b77ea02e46fdb4b9d
Feroz Basheer <feroz@outlook.com>
2020-09-13 07:43:57 -0700

    Refactor kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,20 +2,22 @@
 
 #include <kernel/tty.h>
 #include <kernel/interrupts.h>
+#include <kernel/gdt.h>
 
 void kernel_main(void) {
+	gdt_initialize();
 	idt_initialize();
+
 	terminal_initialize();
+
 	printf("Quilon OS v0.0.1\r\n");
 	printf("\r\n");
 
+	printf("%c %s %d", 'c' , "this", 42);
 
-	//while(1);
-
-	////int i = 1;
-	//while(i) {
-		printf("%c %s %d", 'c' , "this", 42);
-	//}
-
-	while(1);
+	int i = 0;
+	while(i<200)
+	{
+		printf("%d\r\n", i++);
+	}
 }
